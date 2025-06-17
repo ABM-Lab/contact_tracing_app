a1dcabdd7dca4fa44e5dadc2532e0e6ff0116bc6
Taiye <tkommixx@gmail.com>
2025-06-17 15:54:48 -0400

    Removed scen from run_param_scen_cal

--- scen.jl
+++ scen.jl
@@ -1,4 +1,4 @@
-# Taiye (2025.05.31)
+# Taiye (2025.06.17)
 
 # Taiye (2025.05.30): run_param_scen_cal has 16 inputs here but 21 in simulations_cluster.jl.
 
@@ -9,4 +9,4 @@
 #0.35 for omicron generates a R0 of 0.84
 #b::Float64,province::String="ontario",ic1::Int64=1,index::Int64 = 0,scen::Int64 = 0,test_time::Int64=0,test_dur::Int64=0,mt::Int64=300,nsims::Int64=500)
 cv.ModelParameters(popsize=10)
-run_param_scen_cal(0.1,"ontario",1,0,0,10,50,200,2,1000)
+run_param_scen_cal(0.1,"ontario",1,0,10,50,200,2,1000)
